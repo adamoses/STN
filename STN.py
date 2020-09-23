3accb14460aea36bf3df0b903171bf7a01a0dc21
Adam Moses <amoses@vassar.edu>
2020-09-22 21:13:43 -0400

    Update STN.py

--- STN.py
+++ STN.py
@@ -1,9 +1,6 @@
-for each X, d(x) := 0
-    for i=1 to (n-1),
-        for each edge (U,δ,V) in graph,
-            d(v) := min{d(v), d(U) + δ}
+class STN:
+    def __init__(self, num_tp, num_edges, tp_names):
 
-    for each edge (U,δ,V) in graph,
-        if (d(V) > d(U) δ) return False
-    
-    return true
+        self.num_tp = num_tp
+        self.num_edges = num_edges
+        self.tp_names = tp_names
